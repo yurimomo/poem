ff350c88d470cac4ac46a79be5aa740735a6ae02
一宮百合子 <ichinomiyayuriko@mba.local>
2017-08-24 17:27:57 +0900

    バグ修正済み

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -41,6 +41,7 @@ class PoemsController < ApplicationController
   # PATCH/PUT /poems/1.json
   def update
     respond_to do |format|
+      @poem = Poem.find(params[:id])
       if @poem.update(poem_params)
         format.html { redirect_to @poem, notice: 'Poem was successfully updated.' }
         format.json { render :show, status: :ok, location: @poem }
@@ -69,6 +70,6 @@ class PoemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poem_params
-      params.require(:poem).permit(:content, :auther)
+      params.require(:poem).permit(:title ,:content, :auther)
     end
 end
