560e09af3fa1bc58e2bf707cc990414d1c7ea633
一宮百合子 <ichinomiyayuriko@mba.local>
2017-08-25 15:40:47 +0900

    Topcontroller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :poems ,only: [:show, :update, :destroy, :edit, :new, :create]
-  root 'poems#index'
+  get 'top/index'
+
+  resources :poems ,only: [:index, :show, :update, :destroy, :edit, :new, :create]
+  
+
+  root 'top#index'
 end
